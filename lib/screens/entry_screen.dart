a8534099defa6fb21bda49e2033d9da8a238c35c
HYEJIN <imagine@kookmin.ac.kr>
2024-03-24 18:57:46 +0900

    Feat: Entry Screen UI 구현
    텍스트로만 이루어진 간단한 Entry Screen을 구현하였습니다.

--- lib/screens/entry_screen.dart
+++ lib/screens/entry_screen.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+class EntryScreen extends StatelessWidget {
+  const EntryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'CLIVING',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 70,
+                  fontWeight: FontWeight.w800,
+                  letterSpacing: -3),
+              textAlign: TextAlign.end,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
