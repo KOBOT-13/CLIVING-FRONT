1ae876e6fc8767866e4e164b84ce310b398bec4d
HYEJIN <imagine@kookmin.ac.kr>
2024-04-03 02:42:41 +0900

    Feat: Record Screen Appbar 생성
    - Event 객체와 selectedDay를 인자로 받도록 설정했습니다.
    - 앱바에는 selectedDay가 년,월,일로 출력되어 보여집니다.
    - 앱바의 좌측 상단 arrow Icon으로 '뒤로가기'를 구현했습니다.

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -1,8 +1,11 @@
+import 'package:cliving_front/screens/event.dart';
 import 'package:flutter/material.dart';
 
 class RecordScreen extends StatefulWidget {
-  const RecordScreen({super.key});
-
+  DateTime selectedDay;
+  Event selectedEvent;
+  RecordScreen(
+      {super.key, required this.selectedDay, required this.selectedEvent});
   @override
   State<RecordScreen> createState() => _RecordScreenState();
 }
@@ -10,6 +13,57 @@ class RecordScreen extends StatefulWidget {
 class _RecordScreenState extends State<RecordScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int year = widget.selectedDay.year;
+    int month = widget.selectedDay.month;
+    int day = widget.selectedDay.day;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+        // elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_rounded,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          '$year년 $month월 $day일',
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Selected Day: ${widget.selectedDay.toString()}',
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Selected Event: ${widget.selectedEvent.place}',
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Event Color: ${widget.selectedEvent.color}',
+                style: const TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
