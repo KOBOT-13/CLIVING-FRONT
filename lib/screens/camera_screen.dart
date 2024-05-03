221a6018de996da9cb797efdc478a6d5a3441dd5
rktlskan021 <rktlskan021@gmail.com>
2024-05-03 17:04:21 +0900

    feat: 카메라 구현
    Camera 라이브러리 설치 후 설정
    camera_screen 추가
    디바이스에서 사용가능한 카메라를 찾아서 사용하도록 구현

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({super.key});
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller;
+  Future<void>? _initializeControllerFuture;
+  late List<CameraDescription> _cameras;
+
+  @override
+  void initState(){
+    super.initState();
+    findCameras();
+  }
+  Future<void> findCameras() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _cameras = await availableCameras();
+    setCamera();
+    setState(() {});
+  }
+
+  void setCamera(){
+    if (_cameras.isNotEmpty) {
+      _controller = CameraController(_cameras[0], ResolutionPreset.max, enableAudio: false);
+      _initializeControllerFuture = _controller?.initialize().catchError((Object e) {
+        if (e is CameraException) {
+          switch (e.code) {
+            case 'CameraAccessDenied':
+              print("CameraController Error : CameraAccessDenied");
+              break;
+            default:
+              print("CameraController Error");
+              break;
+          }
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller == null || _initializeControllerFuture == null) {
+      return Container(); // 카메라가 없거나 초기화에 실패한 경우
+    }
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return CameraPreview(_controller!);
+        } else {
+          return Center(child: CircularProgressIndicator()); // 초기화 중 로딩 인디케이터
+        }
+      },
+    );
+  }
+}
