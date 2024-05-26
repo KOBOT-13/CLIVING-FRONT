dec0539559c5d1bf070b77127907b524d9c84c66
HYEJIN <imagine@kookmin.ac.kr>
2024-05-26 17:23:11 +0900

    Feat: .env 파일 관련 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'screens/main_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
