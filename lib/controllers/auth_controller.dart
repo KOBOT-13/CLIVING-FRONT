2b719aca1b892b565cf8a3ddd9722556440b185a
HYEJIN <imagine@kookmin.ac.kr>
2024-11-04 00:13:34 +0900

    Chore: Update Podfile.lock for dependency management

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -16,9 +16,9 @@ class AuthController extends GetxController {
     await _storage.write(key: 'accessToken', value: access);
     await _storage.write(key: 'refreshToken', value: refresh);
 
-    // // 저장 후 확인용 코드
-    // print("Access Token Saved: ${await _storage.read(key: 'accessToken')}");
-    // print("Refresh Token Saved: ${await _storage.read(key: 'refreshToken')}");
+    // 저장 후 확인용 코드
+    print("Access Token Saved: ${await _storage.read(key: 'accessToken')}");
+    print("Refresh Token Saved: ${await _storage.read(key: 'refreshToken')}");
 
     // AuthController의 상태 변수 업데이트
     accessToken.value = access;
@@ -40,9 +40,9 @@ class AuthController extends GetxController {
     refreshToken.value = await _storage.read(key: 'refreshToken');
     isLoggedIn.value = accessToken.value != null && refreshToken.value != null;
 
-    // // 확인용  콘솔 출력
-    // print("Access Token Loaded: ${accessToken.value}");
-    // print("Refresh Token Loaded: ${refreshToken.value}");
-    // print("Is Logged In: ${isLoggedIn.value}");
+    // 확인용  콘솔 출력
+    print("Access Token Loaded: ${accessToken.value}");
+    print("Refresh Token Loaded: ${refreshToken.value}");
+    print("Is Logged In: ${isLoggedIn.value}");
   }
 }
