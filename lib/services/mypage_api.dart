05f8fb68d9c2f56f03f4dd79e2a018e0b37fd03e
HYEJIN <imagine@kookmin.ac.kr>
2024-11-13 16:35:58 +0900

    Chore: 불필요한 코드 삭제
    닉네임과 프로필 이미지 patch API를 따로 구성하여 구현함에 따라 기존 API 호출 함수를 삭제합니다.

--- lib/services/mypage_api.dart
+++ lib/services/mypage_api.dart
@@ -56,32 +56,4 @@ class UserService {
       return null;
     }
   }
-
-  // Future<Map<String, dynamic>?> updateProfileImage(
-  //     String newProfileImage) async {
-  //   //URL 설정
-  //   final url = Uri.parse('$API_ADDRESS/api/users/profile/update/');
-  //   final accessToken = authController.accessToken.value;
-  //   print("Authorization Header: Bearer $accessToken");
-
-  //   // headers 정의
-  //   Map<String, String> headers = {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': 'Bearer $accessToken',
-  //   };
-
-  //   // body 정의
-  //   Map<String, String> body = {'profile_image': newProfileImage};
-
-  //   final response =
-  //       await http.patch(url, headers: headers, body: jsonEncode(body));
-
-  //   if (response.statusCode == 200) {
-  //     // 성공적으로 사용자 정보 가져옴
-  //     return json.decode(response.body);
-  //   } else {
-  //     print("Failed to patch profileImage : ${response.statusCode}");
-  //     return null;
-  //   }
-  // }
 }
