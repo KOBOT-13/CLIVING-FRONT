6a171fda485f821bfc369166e0f9c4dc013a12c9
HYEJIN <imagine@kookmin.ac.kr>
2024-11-17 16:20:31 +0900

    Feat: 프로필 이미지 변경사항 Storage에도 반영

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -90,4 +90,12 @@ class AuthController extends GetxController {
     print("nickname: ${nickname.value}");
     print("profileImage: ${profileImage.value}");
   }
+
+  Future<void> updateProfileImageInStorage(String newProfileImage) async {
+    // Flutter Secure Storage에 새로운 프로필 이미지 저장
+    await _storage.write(key: 'profileImage', value: newProfileImage);
+    profileImage.value = newProfileImage;
+
+    print("Updated Profile Image in Storage: $newProfileImage");
+  }
 }
