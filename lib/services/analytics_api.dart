6111f47ec95c98f787576d122d46be43dd1da03f
HYEJIN <imagine@kookmin.ac.kr>
2024-11-18 11:13:16 +0900

    Feat: 홀드 색상 데이터 Get API 연결

--- lib/services/analytics_api.dart
+++ lib/services/analytics_api.dart
@@ -60,4 +60,49 @@ class AnalyticsApi {
       throw Exception('Failed to read Annual Time.');
     }
   }
+
+  Future<List<dynamic>> getMonthlyColor(
+      String selectedYear, String selectedMonth) async {
+    final accessToken = authController.accessToken.value;
+    final url = Uri.parse(
+        '$apiAddress/v1/statistics/color-tries/$selectedYear/$selectedMonth/');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> readMonthlyColor =
+          json.decode(utf8.decode(response.bodyBytes));
+      return readMonthlyColor;
+    } else {
+      throw Exception('Failed to read Monthly Color.');
+    }
+  }
+
+  Future<List<dynamic>> getAnnualColor(String selectedYear) async {
+    final accessToken = authController.accessToken.value;
+    final url =
+        Uri.parse('$apiAddress/v1/statistics/color-tries/$selectedYear/');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> readAnnualColor =
+          json.decode(utf8.decode(response.bodyBytes));
+      return readAnnualColor;
+    } else {
+      throw Exception('Failed to read Annual Color.');
+    }
+  }
 }
