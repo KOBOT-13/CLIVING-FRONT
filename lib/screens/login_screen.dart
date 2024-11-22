36c242dcdda7583f88eeb08a64c1e0846fa152c3
rktlskan021 <rktlskan021@gmail.com>
2024-11-22 18:26:44 +0900

    fix login screen ui 버그 수정

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -33,116 +33,124 @@ class _LoginScreenState extends State<LoginScreen> {
     final deviceWidth = MediaQuery.of(context).size.width;
     final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Center(
-            child: Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(top: deviceHeight / 5),
-          child: Image.asset(
-            'assets/images/entry.png',
-            width: 250,
-            height: 250,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
-          child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  id = value;
-                });
-              },
-              decoration: const InputDecoration(
-                  hintText: '아이디',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Colors.black,
-                  )),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Color.fromRGBO(46, 149, 210, 1),
-                    width: 2.0,
-                  )))),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-          child: TextField(
-            onChanged: (value) {
-              setState(() {
-                password = value;
-              });
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
             },
-            obscureText: true,
-            decoration: const InputDecoration(
-                hintText: '비밀번호',
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                  color: Colors.black,
-                )),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                  color: Color.fromRGBO(46, 149, 210, 1),
-                  width: 2.0,
-                ))),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: ElevatedButton(
-            onPressed: () {
-              login();
-            },
-            style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromRGBO(46, 149, 210, 1),
-                minimumSize: Size(deviceWidth, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                )),
-            child: const Text(
-              "로그인",
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 20,
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/join');
+            child: Center(
+                child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: deviceHeight / 5),
+                  child: Image.asset(
+                    'assets/images/entry.png',
+                    width: deviceWidth / 1.5,
+                    height: deviceWidth / 1.5,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+                  child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          id = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                          hintText: '아이디',
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Colors.black,
+                          )),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Color.fromRGBO(46, 149, 210, 1),
+                            width: 2.0,
+                          )))),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
                     },
-                    child: const Text(
-                      "회원가입",
-                      style: TextStyle(
-                        color: Color(0xFF1B1B1B),
-                      ),
-                    )),
-              ),
-              const Text(
-                " | ",
-                style: TextStyle(color: Color(0xFFC8AAAA)),
-              ),
-              Expanded(
-                child: TextButton(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: '비밀번호',
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.black,
+                        )),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color.fromRGBO(46, 149, 210, 1),
+                          width: 2.0,
+                        ))),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: ElevatedButton(
                     onPressed: () {
-                      // Navigator.pushNamed(context, '');
+                      login();
                     },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(46, 149, 210, 1),
+                        minimumSize: Size(deviceWidth, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                     child: const Text(
-                      "비밀번호 찾기",
+                      "로그인",
                       style: TextStyle(
-                        color: Color(0xFF1B1B1B),
+                        color: Colors.black87,
+                        fontSize: 20,
                       ),
-                    )),
-              ),
-            ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/join');
+                            },
+                            child: const Text(
+                              "회원가입",
+                              style: TextStyle(
+                                color: Color(0xFF1B1B1B),
+                              ),
+                            )),
+                      ),
+                      const Text(
+                        " | ",
+                        style: TextStyle(color: Color(0xFFC8AAAA)),
+                      ),
+                      Expanded(
+                        child: TextButton(
+                            onPressed: () {
+                              // Navigator.pushNamed(context, '');
+                            },
+                            child: const Text(
+                              "비밀번호 찾기",
+                              style: TextStyle(
+                                color: Color(0xFF1B1B1B),
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
           ),
-        )
-      ],
-    )));
+        ));
   }
 }
