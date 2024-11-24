a6dfec9a22ccfe343be9571e93a2e83d6fd88d64
rktlskan021 <rktlskan021@gmail.com>
2024-11-24 17:16:44 +0900

    feat: NumberFormatter services로 구성

--- lib/screens/join_screen.dart
+++ lib/screens/join_screen.dart
@@ -4,39 +4,7 @@ import 'package:gap/gap.dart';
 import 'dart:async';
 import '../services/join_api.dart';
 import '../widgets/custom_dialog.dart';
-
-class NumberFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = newValue.text;
-    if (newValue.selection.baseOffset == 0) {
-      return newValue;
-    }
-
-    var buffer = StringBuffer();
-    for (int i = 0; i < text.length; i++) {
-      buffer.write(text[i]);
-      var nonZeroIndex = i + 1;
-      if (nonZeroIndex <= 3) {
-        if (nonZeroIndex % 3 == 0 && nonZeroIndex != text.length) {
-          buffer.write('-'); // Add double spaces.
-        }
-      } else {
-        if (nonZeroIndex % 7 == 0 &&
-            nonZeroIndex != text.length &&
-            nonZeroIndex > 4) {
-          buffer.write('-');
-        }
-      }
-    }
-
-    var string = buffer.toString();
-    return newValue.copyWith(
-        text: string,
-        selection: TextSelection.collapsed(offset: string.length));
-  }
-}
+import '../services/number_formatter.dart';
 
 class JoinScreen extends StatefulWidget {
   const JoinScreen({super.key});
